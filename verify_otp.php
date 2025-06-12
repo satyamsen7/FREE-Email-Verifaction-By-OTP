59f8e243c4655845490f8bfff7d56b640732d304
satyam sen <satyamsen267@gmail.com>
2025-06-12 09:49:26 +0530

    final

---
+++ verify_otp.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Verify OTP</title>
+</head>
+<body>
+    <form action="verify_otp_process.php" method="POST">
+        <label>Enter OTP:</label>
+        <input type="text" name="otp" required><br>
+        <button type="submit">Verify</button>
+    </form>
+</body>
+</html>
